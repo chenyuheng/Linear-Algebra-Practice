411c2fe0401ce91fb36e84056dc991db00ca6b90
chenyuheng <pass@chenyuheng.cn>
2019-12-21 13:30:06 +0800

    initial commit

---
+++ src/Arithmetic.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Arithmetic {
+    public static double[][] times(double[][] matrix, double c) {
+        double[][] newMatrix = matrix.clone();
+        for (int i = 0; i < newMatrix.length; i++) {
+            for (int j = 0 ; j < newMatrix[0].length; j++) {
+                newMatrix[i][j] *= c;
+            }
+        }
+        return newMatrix;
+    }
+
+    public static double[][] times(double c, double[][] matrix) {
+        return times(matrix, c);
+    }
+
+    public static double[][] times(double[][] m1, double[][] m2) throws Exception {
+        if (!(m1.length == m2[0].length && m1[0].length == m2.length)) {
+            throw new Exception("Matrix multiplying error: matrices inconsistency.");
+        }
+        double[][] matrix = new double[m1.length][m2[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] =rowInnerProductCol(m1, m2, i, j);
+            }
+        }
+        return matrix;
+    }
+
+    private static double rowInnerProductCol(double[][] m1, double[][] m2, int rowIndex, int colIndex) {
+        double sum = 0;
+        for (int i = 0; i < m2.length; i++) {
+            sum += m1[rowIndex][i] * m2[i][colIndex];
+        }
+        return sum;
+    }
+}
---
+++ src/GaussianElimination.java
@@ -0,0 +1,57 @@
+public class GaussianElimination {
+    public static final boolean debug = false;
+
+    public static void main(String[] args) throws Exception {
+        double[][] normalMatrix = new double[][]{
+                {2,1,1,5},
+                {4,-6,0,-2},
+                {-2,7,2,9}};
+        double[][] singularMatrix = new double[][]{
+                {1,1,1},
+                {2,2,5},
+                {4,4,8}
+        };
+        double[][] rowExchangeMatrix = new double[][]{
+                {1,1,1},
+                {2,2,5},
+                {4,6,8}
+        };
+        double[][] matrix = normalMatrix;
+        gaussianEliminate(matrix, 0);
+        IO.printMatrix(matrix);
+    }
+
+    public static void gaussianEliminate(double[][] matrix, int subMatrixIndex) throws Exception {
+        if (subMatrixIndex >= matrix.length) {
+            return;
+        }
+        for (int i = subMatrixIndex + 1; i < matrix.length; i++) {
+            double pivot = matrix[subMatrixIndex][subMatrixIndex];
+            if (pivot == 0) {
+                pivot = handleZeroPivot(matrix, subMatrixIndex);
+            }
+            double ratio = matrix[i][subMatrixIndex] / pivot;
+            for (int j = subMatrixIndex; j < matrix[0].length; j++) {
+                matrix[i][j] -= matrix[subMatrixIndex][j] * ratio;
+            }
+        }
+        if (debug) {
+            IO.printMatrix(matrix);
+            System.out.println();
+        }
+        gaussianEliminate(matrix, subMatrixIndex + 1);
+    }
+
+    private static double handleZeroPivot(double[][] matrix, int pivotIndex) throws Exception {
+        for (int i = pivotIndex; i < matrix.length; i++) {
+            if (matrix[i][pivotIndex] != 0) {
+                double[] temp = matrix[i];
+                matrix[i] = matrix[pivotIndex];
+                matrix[pivotIndex] = temp;
+                return matrix[pivotIndex][pivotIndex];
+            }
+        }
+        throw new Exception("matrix cannot be gaussian eliminated: singular form");
+    }
+
+}
